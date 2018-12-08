967221aae3abf01e7781efeac77e5ae26e6c0d5b
codingspiderfox <codingspiderfox@gmail.com>
2018-12-08 23:54:27 +0100

    Add MQTT

--- src/main/java/org/codingspiderfox/JhipsterMqttExampleApp.java
+++ src/main/java/org/codingspiderfox/JhipsterMqttExampleApp.java
@@ -6,13 +6,26 @@ import org.codingspiderfox.config.DefaultProfileUtil;
 import io.github.jhipster.config.JHipsterConstants;
 
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
@@ -95,4 +108,48 @@ public class JhipsterMqttExampleApp {
             contextPath,
             env.getActiveProfiles());
     }
+
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setUserName("guest2");
+        connectOptions.setPassword("guest2".toCharArray());
+        String[] serverURIs = {"tcp://rabbitmq:1883"};
+        connectOptions.setServerURIs(serverURIs);
+        factory.setConnectionOptions(connectOptions);
+        return factory;
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+            new MqttPahoMessageDrivenChannelAdapter("testMqtt", mqttClientFactory(),
+                "test");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println("" + message.getPayload());
+            }
+
+        };
+    }
+
 }
